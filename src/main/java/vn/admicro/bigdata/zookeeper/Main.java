780d6400759e35e6b7074977f9720c24a4f61949
Ngoc Don <kaitoukid2701@gmail.com>
2017-01-18 10:28:45 +0700

    Use ZooKeeper instead of Hadoop Configuration

--- src/main/java/vn/admicro/bigdata/zookeeper/Main.java
+++ src/main/java/vn/admicro/bigdata/zookeeper/Main.java
@@ -6,6 +6,7 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
 
 public class Main {
 
@@ -18,6 +19,15 @@ public class Main {
 		zoo.create("/e_test", "DataTest".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 //		zoo.create("/e_test/test1", "DataTest".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 		
+//		System.out.println(new String(zoo.getData("/kmeans", true, null), "UTF-8"));
+		byte[] raw = zoo.getData("/kmeans", true, null);
+		System.out.println("raw: " + raw);
+		String originalString = new String(raw, "UTF-8");
+		System.out.println("origin: " + originalString);
+		String lines[] = originalString.split("\n");
+		for(int i = 0; i < lines.length; i++){
+			System.out.println(i + ": " + lines[i]);
+		}
 		
 		connection.close();
 		
